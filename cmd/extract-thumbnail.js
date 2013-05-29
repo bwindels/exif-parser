d626a375feb8517fd4bcdcd9e08055888ca87bd9
Bruno Windels <bruno.windels@gmail.com>
2013-05-29 03:30:18 +0200

    make extract thumbnail command use new facade code

--- cmd/extract-thumbnail.js
+++ cmd/extract-thumbnail.js
@@ -1,25 +1,14 @@
-var jpeg = require('../lib/jpeg'),
-	exif = require('../lib/exif'),
-	BufferStream = require('../lib/bufferstream.js');
+var exifParser = require('../lib/index');
 
 var buf = require('fs').readFileSync(process.argv[2]);
-var thumbOffset, thumbLength, app1Offset, jpegStream = new BufferStream(buf), start = jpegStream.mark();
-jpeg.parseSections(jpegStream, function(sectionType, sectionStream) {
-	if(sectionType === 0xE1) {
-		app1Offset = sectionStream.offsetFrom(start);
-		exif.parseTags(sectionStream, function(ifdSection, tagType, value, format) {
-			if(tagType === 0x0201) {
-				thumbOffset = value[0];
-			} else if(tagType === 0x0202) {
-				thumbLength = value[0];
-			}
-		});
-	}
-});
+var parser = exifParser.create(buf).enableReturnTags(false);
+var result = parser.parse();
 
-if(!thumbOffset || !thumbLength) {
-	console.log('no thumbnail in this image');
+if(!result.hasThumbnail('image/jpeg')) {
+	console.log('no jpeg thumbnail in this image');
 } else {
-	thumbOffset += 6 + app1Offset;
-	require('fs').writeFileSync(process.argv[3], buf.slice(thumbOffset, thumbOffset + thumbLength));
+	var size = result.getThumbnailSize();
+	var buffer = result.getThumbnailBuffer();
+	require('fs').writeFileSync(process.argv[3], buffer);
+	console.log('wrote thumbnail of size ' + size.width + 'x' + size.height + ' to ' + process.argv[3] + ' (' + buffer.length + ' bytes)');
 }
