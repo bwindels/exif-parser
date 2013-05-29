b2a81c6e43d40d0dee8fcd66898e2af3fe7313ef
Bruno Windels <bruno.windels@gmail.com>
2013-05-29 04:22:42 +0200

    add status codes to extract-thumbnail command

--- cmd/extract-thumbnail.js
+++ cmd/extract-thumbnail.js
@@ -6,9 +6,11 @@ var result = parser.parse();
 
 if(!result.hasThumbnail('image/jpeg')) {
 	console.log('no jpeg thumbnail in this image');
+	process.exit(1);
 } else {
 	var size = result.getThumbnailSize();
 	var buffer = result.getThumbnailBuffer();
 	require('fs').writeFileSync(process.argv[3], buffer);
 	console.log('wrote thumbnail of size ' + size.width + 'x' + size.height + ' to ' + process.argv[3] + ' (' + buffer.length + ' bytes)');
+	process.exit(0);
 }
