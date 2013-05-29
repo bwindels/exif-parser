b31d67df430fd15d7f3f09ebca930448f778d542
Bruno Windels <bruno.windels@gmail.com>
2013-05-29 03:40:27 +0200

    yes we need to do this since we use global in the next line

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ function getGlobal() {
 
 module.exports = {
 	create: function(buffer, global) {
+		global = global || getGlobal();
 		if(buffer instanceof global.ArrayBuffer) {
 			var DOMBufferStream = require('./lib/dom-bufferstream');
-			return new Parser(new DOMBufferStream(buffer, 0, buffer.length, true, global || getGlobal()));
+			return new Parser(new DOMBufferStream(buffer, 0, buffer.length, true, global));
 		} else {
 			var NodeBufferStream = require('./lib/bufferstream');
 			return new Parser(new NodeBufferStream(buffer, 0, buffer.length, true));
