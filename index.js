4b5d0e9637e465479cc8a6cfdb527f245d712c81
Bruno Windels <bruno.windels@gmail.com>
2013-06-01 16:56:43 +0200

    fix another bug with dom stream

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
 		global = global || getGlobal();
 		if(buffer instanceof global.ArrayBuffer) {
 			var DOMBufferStream = require('./lib/dom-bufferstream');
-			return new Parser(new DOMBufferStream(buffer, 0, buffer.length, true, global));
+			return new Parser(new DOMBufferStream(buffer, 0, buffer.byteLength, true, global));
 		} else {
 			var NodeBufferStream = require('./lib/bufferstream');
 			return new Parser(new NodeBufferStream(buffer, 0, buffer.length, true));
