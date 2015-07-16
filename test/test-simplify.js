962edc85f154cef84b1ff6d5d7c7e6611487741b
Trevon <trevon.romanuik@gmail.com>
2015-07-16 09:20:48 -0600

    added ModifyDate to list of date fields to simplify

--- test/test-simplify.js
+++ test/test-simplify.js
@@ -4,7 +4,8 @@ module.exports = {
 	'test castDateValues': function(test) {
 		var values = {
 			'DateTimeOriginal': '1970:01:01 00:00:00',
-			'CreateDate': '1970-01-01T00:00:00-05:00'
+			'CreateDate': '1970-01-01T00:00:00-05:00',
+			'ModifyDate': '1970-01-01T00:00:00-05:00'
 		};
 		var setValues = {};
 		function getTagValue(tag) {
@@ -17,6 +18,7 @@ module.exports = {
 		test.strictEqual(Object.keys(setValues).length, 2);
 		test.strictEqual(setValues.DateTimeOriginal, 0);
 		test.strictEqual(setValues.CreateDate, 5 * 3600);
+		test.strictEqual(setValues.ModifyDate, 5 * 3600);
 		test.done();
 	}
 }
